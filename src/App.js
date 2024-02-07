99eb353ab493403aafce6ffbd582af296364b4d1
Ranjeet1508 <danavrj1998@gmail.com>
2024-02-07 18:08:26 +0530

    frontend

--- src/App.js
+++ src/App.js
@@ -7,34 +7,8 @@ import Navbar from './Component/Navbar/Navbar';
 import AddPatient from './Pages/AddPatient/AddPatient';
 import Details from './Pages/Details';
 import UpdatePatient from './Pages/UpdatePatient';
-import axios from 'axios';
-import { useEffect } from 'react';
 
 function App() {
-  const loadUser = async () => {
-    try {
-      let token = localStorage.getItem("token")
-  
-      const config = {
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
-      };
-      const response = await axios.get(`http://localhost:8080/ehrUser/me`, config);
-      console.log(response.data);
-      console.log('ranjeet');
-    } catch (error) {
-      console.log(error)
-    }
-  }
-  
-
-  useEffect(() => {
-    if (window.performance.navigation.type === 1) {
-      loadUser();
-    }
-    console.log(localStorage.getItem("token"));
-  }, [])
 
   return (
     <div className="App">
